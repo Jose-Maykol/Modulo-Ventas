b1bfc3e6b73693bd8adc8b922e5f62ce7b66b66a
José Maykol <jpaniura@unsa.edu.pe>
2021-11-11 15:35:25 -0500

    CRUD ventas terminado

--- sales/urls.py
+++ sales/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.VentaCreateView.as_view(), name= 'create_sale')
+    path('create/', views.VentaCreateView.as_view(), name= 'create_sale'),
+    path('list/', views.VentaListView.as_view(), name='list_sale'),
+    path('edit/<int:pk>/', views.VentaUpdateView.as_view(), name='update_sale'),
+    path('delete/<int:pk>/', views.VentaDeleteView.as_view(), name='delete_sale'),
 ]
--- sales/views.py
+++ sales/views.py
@@ -8,23 +8,27 @@ from .forms import VentaForm
 class VentaCreateView(CreateView):
     model = Venta
     form_class = VentaForm
-    template_name = "create_customer.html"
+    template_name = "create_sale.html"
     success_url = '/'
 
 
 class VentaListView(ListView):
     model = Venta
-    template_name = "TEMPLATE_NAME"
+    template_name = "list_sale.html"
 
 
 class VentaUpdateView(UpdateView):
     model = Venta
-    template_name = "TEMPLATE_NAME"
+    form_class = VentaForm
+    field = '__all__'
+    template_name = "update_sale.html"
+    success_url = '/sales/list/'
 
 
-class VentaDetailView(DetailView):
+class VentaDeleteView(DeleteView):
     model = Venta
-    template_name = "TEMPLATE_NAME"
+    template_name = "delete_sale.html"
+    success_url = '/sales/list/'
 
 
 
